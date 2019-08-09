f3dc65a197e01a042cf1d29951613b0efa02f52e
Sebastian Strobl <Sebastian@Strobl.net>
2019-08-09 08:30:39 +0200

    init (only) components and service

--- angular-frontend-demo/src/app/app.module.ts
+++ angular-frontend-demo/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoItemComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
